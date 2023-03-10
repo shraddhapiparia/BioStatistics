2360a0a08e7639549edd34af8815b1f019b6c79d
Shraddha Piparia <shraddhapiparia@gmail.com>
2023-03-09 19:51:27 -0800

    Update Distribution Functions.R

--- Resources/Distribution Functions.R
+++ Resources/Distribution Functions.R
@@ -5,3 +5,12 @@ barplot(dbinom(0:40, size=40, prob=0.3))
 
 pnorm() # probability norm
 qnorm(c(0.05,0.95)) # quantile norm z score
+
+2. Student t confidence interval
+
+xbar=2.4; s=1.8; n=20
+sem = s/sqrt(n)
+me = qt(0.975, df=n-1)*sem # margin of error c(xbar, xbar-me, xbar+me) # print estimate + 95% CI
+
+3. One sample t test
+t.test(pa.dm2$Charlson)
