c59af49702aa4cb2d5005dfd04ba4f9cbfd2a3bc
Shraddha Piparia <shraddhapiparia@gmail.com>
2023-03-11 19:58:24 -0800

    Create summary.R

---
+++ Resources/summary.R
@@ -0,0 +1,24 @@
+1. Data summaries, tables, graphs
+
+1.1 Categorical variables:
+Tabulation (n, %)
+tab = table(data$col) # values
+prop.table(tab) # proportion
+
+Cross-tabulation (n, %)
+tab = table(data$col1, data$col2) # values
+prop.table(tab) # proportion
+
+Barplots and clustered barplots
+
+
+1.2 Continuous variables:
+Five point summary
+
+Mean
+
+Standard deviation
+
+1.3 Analyses steps:
+Read datasets
+
